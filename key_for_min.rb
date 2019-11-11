b3266f9a3f3417554267df86bec506fcd052590f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-11 11:13:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,22 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-
+    new_value = 0
+    new_name = ""
+  
+  name_hash.collect do |name, value|
+    if value < new_value || new_value = 0
+      new_value = value
+      new_name = name
+    end
+  end
+  new_name
+  
+  
+  def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |k, v|
+  hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
